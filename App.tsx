6c374e3d54414393ab435fac0be37406478aa829
shantanu-cleartax <mahaleshantanu3@gmail.com>
2024-02-23 23:41:31 +0530

    navigating between screens

--- App.tsx
+++ App.tsx
@@ -1,23 +1,40 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Button, Text, View} from 'react-native';
 
 function App(): React.JSX.Element {
   const Stack = createNativeStackNavigator();
 
-  function HomeScreen() {
+  function HomeScreen({navigation}: {navigation: any}) {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => navigation.navigate('Details')}
+        />
       </View>
     );
   }
 
-  function DetailsScreen() {
+  function DetailsScreen({navigation}: {navigation: any}) {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => navigation.push('Details')}
+        />
+        <Button
+          title="Go to Home"
+          onPress={() => navigation.navigate('Home')}
+        />
+        <Button title="Go back" onPress={() => navigation.goBack()} />
+        <Button
+          title="Go back to first screen in stack"
+          onPress={() => navigation.popToTop()}
+        />
       </View>
     );
   }
